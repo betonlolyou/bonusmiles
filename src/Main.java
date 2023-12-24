00f58b07f091f33b4ee4573e13ca0f0d3065bac2
betonlolyou <betonlolyou@gmail.com>
2023-12-24 11:26:32 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 13676;
+        int miles = (int) (price / 20);
+        System.out.println("Начислят мили " + miles);
+    }
+}
